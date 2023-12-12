dd223281967487afcc60d271ec13fa3406c5a5ee
Florian <florian@ROGER>
2023-12-12 16:49:12 +0100

    beaucoup de chose

--- scan.sh
+++ scan.sh
@@ -1,5 +1,5 @@
 export SONAR_HOST=172.17.0.2:9000
-export SONAR_TOKEN=
+export SONAR_TOKEN=7a368f9fbfe57e6eeed1546cbe7211ce2c6c5d7b
 export REPO=$(pwd)
 export CONFIG_FILE=$(pwd)/sonar-project.properties
 
